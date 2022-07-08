ab49e84930237a9a4a01eca4cc7b653ac164fe99
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-07-07 21:33:58 -0400

    adding typeing animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,5 +13,19 @@ $('.menubtn').click(function(){
     $('.menubtn i').toggleClass("active");
 });
 
+var typed = new Typed(".typing",{
+    strings:["Web Developer", "Backend Developer", "Computer Engineering"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+var typed = new Typed(".typing2",{
+    strings:["HTML","CSS","MySQL","MERN"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+
 
 });
